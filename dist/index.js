d5235ff8a14c47bff728ffc530e2da216f90a852
Raj Dave <rajdave03rh@gmail.com>
2026-04-27 15:58:42 +0530

    performs update

--- dist/index.js
+++ dist/index.js
@@ -12,6 +12,13 @@ const buildTask = (task, totalTasks) => {
         createdAt: new Date(),
     };
 };
+let allTasks;
+if (fs.existsSync(taskFile)) {
+    allTasks = JSON.parse(fs.readFileSync(taskFile, "utf8"));
+}
+else {
+    allTasks = [];
+}
 switch (command) {
     case undefined:
         const welcomeMsg = await figlet("Welcome !!");
@@ -19,8 +26,8 @@ switch (command) {
         setTimeout(() => rainbow.stop(), 5000); // Stop after 5 seconds
         break;
     case "add":
-        const task = args[1];
-        if (!task) {
+        const taskDescription = args[1];
+        if (!taskDescription) {
             // console.log("Task is required");
             const errorMsg = chalkAnimation.rainbow("Task is required");
             await new Promise((resolve) => setTimeout(resolve, 2000));
@@ -31,14 +38,13 @@ switch (command) {
         if (fs.existsSync(taskFile)) {
             listOfTask = JSON.parse(fs.readFileSync(taskFile, "utf8"));
         }
-        const newTask = buildTask(task, listOfTask.length);
+        const newTask = buildTask(taskDescription, listOfTask.length);
         listOfTask.push({ ...newTask, updatedAt: newTask.createdAt });
         fs.writeFileSync(taskFile, JSON.stringify(listOfTask, null, 2));
         console.log(`Task added successfully (ID: ${newTask.id})`);
         break;
     case "list":
         const statusFilters = args.slice(1);
-        const allTasks = JSON.parse(fs.readFileSync(taskFile, "utf8"));
         let filteredTasks = [];
         if (statusFilters.length > 0) {
             filteredTasks = allTasks.filter((task) => statusFilters.includes(task.status));
@@ -48,6 +54,33 @@ switch (command) {
         }
         filteredTasks.map((task) => console.log(`${task.id}. ${task.description}` +
             `${statusFilters.length > 0 ? ` (${task.status})` : ""}`));
+        break;
+    case "update":
+        const taskId = args[1];
+        if (!taskId) {
+            const errorMsg = chalkAnimation.rainbow("Task ID is required");
+            await new Promise((resolve) => setTimeout(resolve, 2000));
+            errorMsg.stop();
+            process.exit(1);
+        }
+        const task = allTasks.find((task) => task.id === Number(taskId));
+        if (!task) {
+            const errorMsg = chalkAnimation.rainbow("Task not found");
+            await new Promise((resolve) => setTimeout(resolve, 2000));
+            errorMsg.stop();
+            process.exit(1);
+        }
+        const updatedTaskDescription = args[2];
+        if (!updatedTaskDescription) {
+            const errorMsg = chalkAnimation.rainbow("Task description is required");
+            await new Promise((resolve) => setTimeout(resolve, 2000));
+            errorMsg.stop();
+            process.exit(1);
+        }
+        task.description = updatedTaskDescription;
+        task.updatedAt = new Date();
+        fs.writeFileSync(taskFile, JSON.stringify(allTasks, null, 2));
+        console.log(`Task updated successfully (ID: ${taskId})`);
         break;
     default:
         console.error("Command not found");
--- src/index.ts
+++ src/index.ts
@@ -27,17 +27,23 @@ const buildTask = (
   };
 };
 
+let allTasks: Task[];
+if (fs.existsSync(taskFile)) {
+  allTasks = JSON.parse(fs.readFileSync(taskFile, "utf8"));
+} else {
+  allTasks = [];
+}
+
 switch (command) {
   case undefined:
     const welcomeMsg = await figlet("Welcome !!");
     const rainbow = chalkAnimation.rainbow(welcomeMsg);
     setTimeout(() => rainbow.stop(), 5000); // Stop after 5 seconds
-
     break;
 
   case "add":
-    const task = args[1];
-    if (!task) {
+    const taskDescription = args[1];
+    if (!taskDescription) {
       // console.log("Task is required");
       const errorMsg = chalkAnimation.rainbow("Task is required");
       await new Promise((resolve) => setTimeout(resolve, 2000));
@@ -48,7 +54,7 @@ switch (command) {
     if (fs.existsSync(taskFile)) {
       listOfTask = JSON.parse(fs.readFileSync(taskFile, "utf8"));
     }
-    const newTask = buildTask(task, listOfTask.length);
+    const newTask = buildTask(taskDescription, listOfTask.length || -1);
 
     listOfTask.push({ ...newTask, updatedAt: newTask.createdAt });
     fs.writeFileSync(taskFile, JSON.stringify(listOfTask, null, 2));
@@ -57,7 +63,6 @@ switch (command) {
     break;
   case "list":
     const statusFilters = args.slice(1);
-    const allTasks: Task[] = JSON.parse(fs.readFileSync(taskFile, "utf8"));
 
     let filteredTasks: Task[] = [];
     if (statusFilters.length > 0) {
@@ -74,6 +79,39 @@ switch (command) {
           `${statusFilters.length > 0 ? ` (${task.status})` : ""}`,
       ),
     );
+    break;
+  case "update":
+    const taskId = args[1];
+    if (!taskId) {
+      const errorMsg = chalkAnimation.rainbow("Task ID is required");
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      errorMsg.stop();
+      process.exit(1);
+    }
+
+    const task = allTasks.find((task: Task) => task.id === Number(taskId));
+
+    if (!task) {
+      const errorMsg = chalkAnimation.rainbow("Task not found");
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      errorMsg.stop();
+      process.exit(1);
+    }
+
+    const updatedTaskDescription = args[2];
+    if (!updatedTaskDescription) {
+      const errorMsg = chalkAnimation.rainbow("Task description is required");
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      errorMsg.stop();
+      process.exit(1);
+    }
+
+    task.description = updatedTaskDescription;
+    task.updatedAt = new Date();
+
+    fs.writeFileSync(taskFile, JSON.stringify(allTasks, null, 2));
+
+    console.log(`Task updated successfully (ID: ${taskId})`);
     break;
   default:
     console.error("Command not found");
